e1160b10bb2f5a92a5717f75a14b0ae89986d0ca
ttechictcenter <92575777+ttechcenter@users.noreply.github.com>
2024-08-11 22:55:08 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import WelcomeMessage from './components/WelcomeMessage';
 import './App.css'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';   
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
       <div className="App">
       <WelcomeMessage />
       </div>
