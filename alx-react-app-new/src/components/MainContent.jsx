1c2f3616b8877b3dfcbbf588ea998039b040d7ce
ttechictcenter <92575777+ttechcenter@users.noreply.github.com>
2024-08-18 22:17:29 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MainContent() {
   return (
     <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+      <p style={{ color: 'blue' }}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
