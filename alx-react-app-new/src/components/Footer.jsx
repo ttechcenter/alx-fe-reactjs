8b8bfb731314df2b1c5d88c8f8426a0330f03cc7
ttechictcenter <92575777+ttechcenter@users.noreply.github.com>
2024-08-18 22:17:10 +0300

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>© 2023 City Lovers</p>
+      <p style={{ color: 'blue' }}>© 2023 City Lovers</p>
     </footer>
   );
 }
