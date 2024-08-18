5d53d60e87401f8112577cdbf3852d3035828cb3
ttechictcenter <92575777+ttechcenter@users.noreply.github.com>
2024-08-18 22:27:41 +0300

    Add files via upload

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p style={{ color: 'blue' }}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <p style={{ color: 'blue' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
