933a2729ff7fcfd75b8ef48dfc2a3dc8fd9843e6
ttechictcenter <92575777+ttechcenter@users.noreply.github.com>
2024-08-22 19:10:38 +0300

    Update RecipeList.js

--- recipe-sharing-app/src/RecipeList.js
+++ recipe-sharing-app/src/RecipeList.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { useRecipeStore } from './recipeStore';
+  import { useRecipeStore } from './recipeStore';
 
-const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const RecipeList = () => {
+    const recipes = useRecipeStore(state => state.recipes);
 
-  return (
-    <div>
-      <h2>My Recipes</h2>
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-          {/* Add additional display logic for ingredients and instructions (optional) */}
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default RecipeList;
+    return (
+      <div>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
