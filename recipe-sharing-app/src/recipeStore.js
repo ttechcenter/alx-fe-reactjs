709afd8706a4d407e2f26f30567e5682db2d332a
ttechictcenter <92575777+ttechcenter@users.noreply.github.com>
2024-08-22 18:33:00 +0300

    Create recipeStore.js

---
+++ recipe-sharing-app/src/recipeStore.js
@@ -0,0 +1,12 @@
+import create from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [ // Initial state with optional predefined recipes
+    // { id: 1, title: "Pizza", description: "Delicious Italian dish" },
+  ],
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
