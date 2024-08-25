ac630a58db51c0892ff191a466ecb23b5fd987ea
ttechictcenter <92575777+ttechcenter@users.noreply.github.com>
2024-08-25 21:16:59 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -1,12 +1,13 @@
 import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
-  recipes: [ // Initial state with optional predefined recipes
-    // { id: 1, title: "Pizza", description: "Delicious Italian dish" },
-  ],
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  recipes: [],
+  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (recipeId) => set((state) => ({ recipes: state.recipes.filter(recipe => recipe.id !== recipeId) })),
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map(recipe => recipe.id === updatedRecipe.id ? updatedRecipe : recipe)
+  })),
   setRecipes: (recipes) => set({ recipes }),
 }));
 
-export default recipeStore;
+export default useRecipeStore;
