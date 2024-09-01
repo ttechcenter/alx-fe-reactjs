c42a920a7841670c94f5b5032f2154146cce47e9
ttechictcenter <92575777+ttechcenter@users.noreply.github.com>
2024-09-01 22:22:25 +0300

    Add files via upload

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,43 @@
+
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts, {
+    
+        cacheTime: 10 * 60 * 1000, 
+        staleTime: 5 * 60 * 1000, 
+        refetchOnWindowFocus: false,
+        keepPreviousData: true,
+      
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+    </div>
+  );
+};
+
+export default PostsComponent;
