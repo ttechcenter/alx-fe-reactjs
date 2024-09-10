c33b01eb592c662fc8d9c125f769ff581312242a
ttechcenter <92575777+ttechcenter@users.noreply.github.com>
2024-09-10 09:22:04 +0300

    Add files via upload

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import UserProfile from './components/UserProfile'
+
+function App() {
+
+
+  return (
+    <>
+      
+       <UserProfile />
+     
+    </>
+  )
+}
+
+export default App
